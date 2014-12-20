103df177f8518118351b94e7aee59fb581397cb8
Michael Hunter <hunter.michael.k@gmail.com>
2014-12-20 18:30:12 -0500

    The functional test now adds a test for a second to-do item. Now current with book, page 59.

--- functional_tests.py
+++ functional_tests.py
@@ -49,11 +49,9 @@ class NewVisitorTest(unittest.TestCase):
         #)
         self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         
-        self.fail('Finish the test!')
-        
-        edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+')
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        #edith_list_url = self.browser.current_url
+        #self.assertRegex(edith_list_url, '/lists/.+')
+        #self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         #table = self.browser.find_element_by_id('id_list_table')
         #rows = table.find_elements_by_tag_name('tr')
@@ -64,13 +62,22 @@ class NewVisitorTest(unittest.TestCase):
 
         # There is still a text box inviting her to add another item. She 
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        #inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+       
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        
         
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('2: Use Peacock feathers to make a fly')
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        #self.check_for_row_in_list_table('2: Use Peacock feathers to make a fly')
+        #self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        self.fail('Finish the test!')
 
         # Now a new user, Francis, comes along to the site.
         
