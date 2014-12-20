570e42d49e3d1e56a62b619cf6c1d5df271201f3
Michael Hunter <hunter.michael.k@gmail.com>
2014-12-20 18:47:57 -0500

    Refactored test for two-item to-do list to use a helper method. Now consistent with book at page 60.

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
         
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+        
     def test_can_start_a_list_and_retrieve_it_later(self):        
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage.
@@ -34,20 +39,18 @@ class NewVisitorTest(unittest.TestCase):
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        
-
-        
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        #table = self.browser.find_element_by_id('id_list_table')
+        #rows = table.find_elements_by_tag_name('tr')
         #self.assertTrue(
         #    any(row.text == '1: Buy peacock feathers' for row in rows),
         #    "New to-do item did not appear in table -- its text was:\n%s" % (
         #        table.text,
         #    )
         #)
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         
         #edith_list_url = self.browser.current_url
         #self.assertRegex(edith_list_url, '/lists/.+')
@@ -66,16 +69,16 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
+        #table = self.browser.find_element_by_id('id_list_table')
        
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        #rows = table.find_elements_by_tag_name('tr')
+        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        #self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
         
         
         # The page updates again, and now shows both items on her list
-        #self.check_for_row_in_list_table('2: Use Peacock feathers to make a fly')
-        #self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use Peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         self.fail('Finish the test!')
 
