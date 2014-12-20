5b4e19828beb621c1a708f8a4418b011d50a39d6
Michael Hunter <hunter.michael.k@gmail.com>
2014-12-20 18:49:27 -0500

    Removed commented-out code. Now consistent with book at page 60.

--- functional_tests.py
+++ functional_tests.py
@@ -41,27 +41,6 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        
-        #table = self.browser.find_element_by_id('id_list_table')
-        #rows = table.find_elements_by_tag_name('tr')
-        #self.assertTrue(
-        #    any(row.text == '1: Buy peacock feathers' for row in rows),
-        #    "New to-do item did not appear in table -- its text was:\n%s" % (
-        #        table.text,
-        #    )
-        #)
-        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        
-        #edith_list_url = self.browser.current_url
-        #self.assertRegex(edith_list_url, '/lists/.+')
-        #self.check_for_row_in_list_table('1: Buy peacock feathers')
-        
-        #table = self.browser.find_element_by_id('id_list_table')
-        #rows = table.find_elements_by_tag_name('tr')
-        #self.assertTrue(
-        #    any(row.text == '1: Buy peacock feathers' for row in rows),
-        #    "New to-do item did not appear in table"
-        #)
 
         # There is still a text box inviting her to add another item. She 
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -69,13 +48,6 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
 
-        #table = self.browser.find_element_by_id('id_list_table')
-       
-        #rows = table.find_elements_by_tag_name('tr')
-        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        #self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
-        
-        
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('2: Use Peacock feathers to make a fly')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
