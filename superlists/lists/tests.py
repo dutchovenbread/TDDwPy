8f619e519846858ba00d7ecdec37de0c28c8e824
Michael Hunter <hunter.michael.k@gmail.com>
2014-12-23 08:59:01 -0500

    Removed redundant code and one redundant test. Now consistent with book at page 96.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -17,14 +17,6 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('lists/home.html')
         self.assertEqual(response.content.decode(), expected_html)
-        #self.assertTrue(response.content.startswith(b'<html>'))
-        #self.assertIn(b'<title>To-Do lists</title>', response.content)
-        #self.assertTrue(response.content.strip().endswith(b'</html>'))
-       
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
 
@@ -63,15 +55,6 @@ class ListViewTest(TestCase):
 class NewListTest(TestCase):
         
     def test_saving_a_POST_request(self):
-        #request = HttpRequest()
-        #request.method = 'POST'
-        #request.POST['item_text'] = 'A new list item'
-        
-        #response = home_page(request)
-        
-        #self.assertEqual(Item.objects.count(), 1)
-        #new_item = Item.objects.first()
-        #self.assertEqual(new_item.text, 'A new list item')
         self.client.post(
             '/lists/new',
             data={'item_text': 'A new list item'}
@@ -86,20 +69,3 @@ class NewListTest(TestCase):
             data={'item_text': 'A new list item'}
         )
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
-        #self.assertEqual(response.status_code,302)
-        #self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-        #request = HttpRequest()
-        #request.method = 'POST'
-        #request.POST['item_text'] = 'A new list item'
-        
-        #response = home_page(request)
-        
-        #self.assertEqual(response.status_code, 302)
-        #self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')        
-        #self.assertIn('A new list item', response.content.decode())
-        
-        #expected_html = render_to_string(
-        #    'lists/home.html',
-        #    {'new_item_text': 'A new list item'}
-        #)
-        #self.assertEqual(response.content.decode(), expected_html)
