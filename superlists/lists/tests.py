13f8698358fd3d883cd6e124b9006b1c0fdb6da6
Michael Hunter <hunter.michael.k@gmail.com>
2014-12-23 08:23:28 -0500

    Altered new_list view to create a to-do item. Corrected test to properly generate full URL. Now consistent with book at page 95.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -85,8 +85,9 @@ class NewListTest(TestCase):
             '/lists/new',
             data={'item_text': 'A new list item'}
         )
-        self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        #self.assertEqual(response.status_code,302)
+        #self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
         #request = HttpRequest()
         #request.method = 'POST'
         #request.POST['item_text'] = 'A new list item'
